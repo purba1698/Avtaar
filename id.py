f8d3580108ace9eba9376bfe1d1f6bacd2088236
Purba Mandal <purba1698@.com>
2022-01-12 11:30:55 +0530

    updating..

---
+++ id.py
@@ -0,0 +1,7 @@
+import uuid 
+# Printing random id using uuid1() 
+print ("The random id using uuid4() is : ",end="") 
+print (uuid.uuid4()) 
+
+# Output
+# The random id using uuid1() is : 67460e74-02e3-11e8-b443-00163e990bdb
